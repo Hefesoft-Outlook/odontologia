72a80c8627167be5b2c826584052fb764e02df86
chibchombiano@outlook.com <chibchombiano@outlook.com>
2014-10-19 16:43:24 -0500

    Prueba Cnt

--- Cnt.Panacea.Xap.Odontologia.Vm/Grillas/Plan tratamiento/Wizard.cs
+++ Cnt.Panacea.Xap.Odontologia.Vm/Grillas/Plan tratamiento/Wizard.cs
@@ -155,6 +155,7 @@ namespace Cnt.Panacea.Xap.Odontologia.Vm.Grillas.Plan_tratamiento
             OdontologosIps = await Contexto_Odontologia.ListarOdontologosPorIps(Variables_Globales.IdIps);
             HigientistasIps = await Contexto_Odontologia.ListarHigienistasPorIps(Variables_Globales.IdIps);
             
+            //Forzar mostrar combos
             RaisePropertyChanged("OdontologosIps");
             RaisePropertyChanged("HigientistasIps");
         }
