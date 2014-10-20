d7839724f750341fa89f4c756caa8fa787bcc054
chibchombiano@outlook.com <chibchombiano@outlook.com>
2014-10-19 20:16:25 -0500

    Comentarios

--- Cnt.Panacea.Xap.Odontologia.Vm/Grillas/Plan tratamiento/Wizard.cs
+++ Cnt.Panacea.Xap.Odontologia.Vm/Grillas/Plan tratamiento/Wizard.cs
@@ -154,8 +154,7 @@ namespace Cnt.Panacea.Xap.Odontologia.Vm.Grillas.Plan_tratamiento
         {
             OdontologosIps = await Contexto_Odontologia.ListarOdontologosPorIps(Variables_Globales.IdIps);
             HigientistasIps = await Contexto_Odontologia.ListarHigienistasPorIps(Variables_Globales.IdIps);            
-            //Forzar mostrar combos
-            //No solo basta con ser observables
+            
             RaisePropertyChanged("OdontologosIps");
             RaisePropertyChanged("HigientistasIps");
         }
