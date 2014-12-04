7ae8d4b54ddd507c0fc1f634d22a429e7c005b3f
Jose Douglas Ramirez E <chibchombiano26@hotmail.com>
2014-12-04 18:51:29 -0500

    Vsix andando :)

--- Hefesoft/Templates/W8/W8/Wizard/RestTemplateWizard/ChildWizard.cs
+++ Hefesoft/Templates/W8/W8/Wizard/RestTemplateWizard/ChildWizard.cs
@@ -16,9 +16,40 @@ namespace RestTemplateWizard
             Dictionary<string, string> replacementsDictionary, 
             WizardRunKind runKind, object[] customParams)
         {
+
+            nombreProyecto(replacementsDictionary);
+            nombreSinHefesoft(replacementsDictionary);
+
             // Add custom parameters.
             replacementsDictionary.Add("$saferootprojectname$",
                 RootWizard.GlobalDictionary["$saferootprojectname$"]);
+
+        }
+
+        private static void nombreProyecto(Dictionary<string, string> replacementsDictionary)
+        {
+            try
+            {
+                var nombreProyecto = RootWizard.GlobalDictionary["$saferootprojectname$"];
+                nombreProyecto = nombreProyecto.Replace(".Locator", "").Replace(".Test", "");
+                //Declaramos una variable en la que solo van estar el nombre del proyecto para luego hacer los reemplazos
+                replacementsDictionary.Add("$nombreProyecto$", nombreProyecto);
+            }
+            catch 
+            { }
+        }
+
+        private static void nombreSinHefesoft(Dictionary<string, string> replacementsDictionary)
+        {
+            try
+            {
+                var nombreProyectoSinHefesoft = RootWizard.GlobalDictionary["$saferootprojectname$"];
+                nombreProyectoSinHefesoft = nombreProyectoSinHefesoft.Replace(".Locator", "").Replace(".Test", "").Replace(".Hefesoft", "");
+                //Declaramos una variable en la que solo van estar el nombre del proyecto para luego hacer los reemplazos
+                replacementsDictionary.Add("$nombreProyectoSinHefesoft$", nombreProyectoSinHefesoft);
+            }
+            catch
+            { }
         }
 
         public void RunFinished()
